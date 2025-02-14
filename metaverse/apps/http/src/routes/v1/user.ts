dea86f42ff234750293263ebf3c73937698f4fea
Samcode001 <93057570+Samcode001@users.noreply.github.com>
2025-02-14 12:12:48 +0530

    http server is created

---
+++ metaverse/apps/http/src/routes/v1/user.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+export const userRouter = Router();
+
+userRouter.post("/metadata", async (req, res) => {});
+
+userRouter.get("/metadata/bulk", async (req, res) => {});
