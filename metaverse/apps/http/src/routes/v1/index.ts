bfee65acdc621b7aaadb4398e2059bd7b85893c3
Samcode001 <93057570+Samcode001@users.noreply.github.com>
2025-02-14 13:24:39 +0530

    first test succesfully

--- metaverse/apps/http/src/routes/v1/index.ts
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { SigninSchema, SignupSchema } from "../../types";
 import { hash, compare } from "../../scypt";
-// import client from "@repo/db/client";
+import client from "@repo/db/client";
 import jwt from "jsonwebtoken";
 import { userRouter } from "./user";
 import { spaceRouter } from "./space";
@@ -23,15 +23,15 @@ router.post("/signup", async (req, res) => {
   const hashedPassword = await hash(parsedData.data.password);
 
   try {
-    //  const user = await client.user.create({
-    //     data: {
-    //         username: parsedData.data.username,
-    //         password: hashedPassword,
-    //         role: parsedData.data.type === "admin" ? "Admin" : "User",
-    //     }
-    // })
+    const user = await client.user.create({
+      data: {
+        username: parsedData.data.username,
+        password: hashedPassword,
+        role: parsedData.data.type === "admin" ? "Admin" : "User",
+      },
+    });
     res.json({
-      // userId: user.id
+      userId: user.id,
     });
   } catch (e) {
     console.log("erroer thrown");
@@ -48,27 +48,30 @@ router.post("/signin", async (req, res) => {
   }
 
   try {
-    // const user = await client.user.findUnique({
-    //     where: {
-    //         username: parsedData.data.username
-    //     }
-    // })
-    // if (!user) {
-    //     res.status(403).json({message: "User not found"})
-    //     return
-    // }
-    // const isValid = await compare(parsedData.data.password, user.password)
-    // if (!isValid) {
-    //     res.status(403).json({message: "Invalid password"})
-    //     return
-    // }
-    // const token = jwt.sign({
-    //     userId: user.id,
-    //     role: user.role
-    // }, JWT_PASSWORD);
-    // res.json({
-    //     token
-    // })
+    const user = await client.user.findUnique({
+      where: {
+        username: parsedData.data.username,
+      },
+    });
+    if (!user) {
+      res.status(403).json({ message: "User not found" });
+      return;
+    }
+    const isValid = await compare(parsedData.data.password, user.password);
+    if (!isValid) {
+      res.status(403).json({ message: "Invalid password" });
+      return;
+    }
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        role: user.role,
+      },
+      JWT_PASSWORD
+    );
+    res.json({
+      token,
+    });
   } catch (e) {
     res.status(400).json({ message: "Internal server error" });
   }
